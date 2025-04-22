f55f43ac7bd8d4f454f1f1452b9ddc4ad863a4aa
Kurowiczanka <143121641+Kurowiczanka@users.noreply.github.com>
2025-04-22 15:46:53 -0500

    Update FiReMAGE_server_version.R
    Synching this script with the local one.

--- scripts/FiReMAGE_server_version.R
+++ scripts/FiReMAGE_server_version.R
@@ -893,6 +893,13 @@ candidateList <-
     subList$pFDR <- pFDR_backend$pFDR
     return(subList)
   }
+## Calculating GPL value - "How many FM hits of the same x/5 presence are within the same locus as the given gene?"
+candidateList$GPL <- NA
+
+for(i in 1:nrow(candidateList)){
+  candidateList$GPL[i] <- 1 + nrow(candidateList[candidateList$trait==candidateList$trait[i] & candidateList$present==candidateList$present[i] & candidateList$loci==candidateList$loci[i] & candidateList$`Gene Name` != candidateList$`Gene Name`[i],])
+}
+
 
 ## Dividing the candidate list up by trait
 
